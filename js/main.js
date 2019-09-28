0ef1918f6bb6be329a6303228ffaee9e8a886fcd
Артур <Greensolitary@gmail.com>
2019-09-28 21:22:29 +0500

    задание 3

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
 'use strict';
+var similarListElement = document.getElementById('#picture');
+
+var similarPictureTemplate = document.getElementById('#picture').content.querySelector('.picture');
 
 
 var MIN_LIKES = 15;
 var MAX_LIKES = 200;
 var PICTURES_NUM = 25;
 var COMMENTS_MINIMUM_NUM = 1;
-var pictures = [];
 var commentsArray = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
@@ -15,43 +17,59 @@ var commentsArray = [
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
 ];
 
-function generatePictures() {
-  for (var i = 0; i < PICTURES_NUM; i++) {
-    var url = 'photos/' + (i + 1) + '.jpg';
-    var likes = getRandom(MIN_LIKES, MAX_LIKES);
-    var comments = generateComments();
-
-    pictures.push({url: url, likes: likes, comments: comments});
-  }
-}
-
-generatePictures();
-
 function findRandomValue(num) {
   return Math.floor(Math.random() * num);
 }
 
-// function generateLikes() {
-//   var likes = 0;
-//   while (likes < MIN_LIKES || likes > MAX_LIKES) {
-//     likes = Math.floor(Math.random() * MAX_LIKES);
-//   }
-//   return (likes);
-// }
-
-var getRandom = function randomInteger(min, max) {
+function randomInteger(min, max) {
   var rand = min + Math.random() * (max + 1 - min);
   return Math.floor(rand);
-};
+}
 
 function generateComments() {
   var comments = [];
   var commentsNum = findRandomValue(20) + COMMENTS_MINIMUM_NUM;
   for (var i = 0; i < commentsNum; i++) {
-    comments.push(commentsArray[Math.floor(Math.random() * commentsArray.length)]);
+    comments.push(commentsArray[Math.floor(findRandomValue() * commentsArray.length)]);
     if (findRandomValue(MAX_LIKES) % 2 !== 0) {
-      comments[i] = comments[i] + ' ' + commentsArray[Math.floor(Math.random() * commentsArray.length)];
+      comments[i] = comments[i] + ' ' + commentsArray[Math.floor(findRandomValue() * commentsArray.length)];
     }
   }
-  return (comments);
+  return comments;
 }
+
+function generatePictures() {
+  var pictures = [];
+
+  for (var i = 0; i < PICTURES_NUM; i++) {
+    var url = 'photos/' + (i + 1) + '.jpg';
+    var likes = randomInteger(MIN_LIKES, MAX_LIKES);
+    var comments = generateComments();
+
+    pictures.push({url: url, likes: likes, comments: comments});
+  }
+
+  return pictures;
+}
+
+var picturesMocks = generatePictures(picturesMocks);
+
+function createPicture(picture) {
+  var pictureElement = similarPictureTemplate.cloneNode(true);
+
+  pictureElement.querySelector('.picture__img').url = picture.url;
+  pictureElement.querySelector('.picture__likes').textContent = picture.likes;
+  pictureElement.querySelector('.picture__comments').textContent = picture.comments;
+
+  return pictureElement;
+}
+
+function renderPictures(arr) {
+  var fragment = renderPictures(picturesMocks);
+  for (var i = 0; i < arr.length; i++) {
+    fragment.appendChild(createPicture(arr[i]));
+  }
+  similarListElement.appendChild(fragment);
+}
+
+renderPictures();
